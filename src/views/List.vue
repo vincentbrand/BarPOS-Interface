053e29cba011fab377206131d888a5f638930efd
李鹏程 <2724392356@qq.com>
2021-01-29 10:52:09 +0800

    增加关闭订单功能

--- src/views/List.vue
+++ src/views/List.vue
@@ -38,7 +38,7 @@
                       <i class="fas fa-search"></i> Details
                     </button>
 
-                    <button class="btn btn-info float-right">
+                    <button class="btn btn-info float-right" @click="openOtherPay(user)">
                       <i class="fas fa-yen-sign"></i> Manual Pay
                     </button>
 
@@ -124,6 +124,7 @@
     </div>
     <!--/. container-fluid -->
 
+    <!-- 微信  支付宝 -->
     <el-dialog
     title="QR Payment"
     :visible.sync="isShowPayCode"
@@ -146,9 +147,28 @@
         </div>
 
         <span slot="footer" class="dialog-footer">
-            <el-button @click="isShowPayCode = false">Close</el-button>
+            <button class="btn btn-info" @click="isShowPayCode = false">Close</button>
         </span>
     </el-dialog>
+
+    <!-- 其他支付方式 -->
+    <el-dialog
+    title="Method of Payment"
+    :visible.sync="isShowOtherPay"
+    width="30%">
+        <div class="row">
+            <div class="col-6 text-center">
+              <button class="btn btn-info" @click="closePayBills">
+                Cash Payments
+              </button>
+            </div>
+            <div class="col-6 text-center">
+              <button class="btn btn-info" @click="closePayBills">
+                Unionpay Pay
+              </button>
+            </div>
+        </div>
+  </el-dialog>
 
   </section>
   
@@ -162,9 +182,11 @@ export default {
 
   data: () => ({
     isShowPayCode: false,
+    isShowOtherPay: false,
     userIdx: "",
     categoryTabIdx: 0,
     emloyeeId: 0,
+    closeOtherPayId: 0,
     weChatQrCode: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAABlBMVEX///8AAABVwtN+AAACjElEQVR4nO3dS47bQAwE0Jn7XzqrAAEit1hFGcYkj8s2P/V0AX997+rrd0U9X3/V5Kcy4Xae8LKHkDBKuJ0nvOwhJIwSHlYfqssafY5o4WmK8G6+CkQYLzxNEd7NV4EI44WnKcK7+SrQh4WT893Vw3h0K0tISEhISEhI+Jxw+RUICQkJCQkJf5bwAHvqJ0JCQkJCQsK9cFKROdo8uZVNEU6vEvZFeDdFOL1K2Bfh3dT3rpbm6KVMuJ0nfPClTLidJ3zwpUy4nSd88KVMuJ0nfPBlmXBb0eoJ47FkTxXhdXM3/pkivG7uxj9ThNfN3fhnivC6uRsvky0DTSI+1RyNExISEhISEl66Jp7Joqh5Ul2M06LlasK4CAknyQinewpeHeO0aLmaMK5SGEWcHIv2PMUgJIxyZOeTzYSE3U+Ed+eTzYR3Jw4bu6tdT/TtDuOEhISEhISEf05NYNHG5XhkHn0OQkJCQkJCwjD0af71wslL91OUh5CQkJCQkLBONtnTVZmekJCQkJCQ8Dnh4wsf/4jbQFER9kcJF4GiIuyPEi4CRUXYHy2F74s4yXEIfRjfUgkJCQkJCQlnFa2O0h/GlzGyIrxpJrweX8bIivCmmfB6fBkjK8Kb5jcKJ8cmOSYv0ZeavBzynNCEhISEhIT/sXCZY5n1jZsJCQkJCQkJ3/B/wFGg6Ho0lZ0nJCQkJCQknAmj9JOpSYzRFyckJCQkJCR8p7BLP7kefVbCF2OHq9HCqAhfNEeBCAl3RfiiOQr0jwgntby6PHpYSJgfWwbqjhISRseWgbqjhITRsWWg7mgp7OoQOsoxaS4TbucJL1+65jLhdp7w8qVrLhNu5wkvX7rmMuF2nvDypWvu6hdwJWEkXvHKCAAAAABJRU5ErkJggg==',
     AlipayQrCode: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAABlBMVEX///8AAABVwtN+AAACjElEQVR4nO3dS47bQAwE0Jn7XzqrAAEit1hFGcYkj8s2P/V0AX997+rrd0U9X3/V5Kcy4Xae8LKHkDBKuJ0nvOwhJIwSHlYfqssafY5o4WmK8G6+CkQYLzxNEd7NV4EI44WnKcK7+SrQh4WT893Vw3h0K0tISEhISEhI+Jxw+RUICQkJCQkJf5bwAHvqJ0JCQkJCQsK9cFKROdo8uZVNEU6vEvZFeDdFOL1K2Bfh3dT3rpbm6KVMuJ0nfPClTLidJ3zwpUy4nSd88KVMuJ0nfPBlmXBb0eoJ47FkTxXhdXM3/pkivG7uxj9ThNfN3fhnivC6uRsvky0DTSI+1RyNExISEhISEl66Jp7Joqh5Ul2M06LlasK4CAknyQinewpeHeO0aLmaMK5SGEWcHIv2PMUgJIxyZOeTzYSE3U+Ed+eTzYR3Jw4bu6tdT/TtDuOEhISEhISEf05NYNHG5XhkHn0OQkJCQkJCwjD0af71wslL91OUh5CQkJCQkLBONtnTVZmekJCQkJCQ8Dnh4wsf/4jbQFER9kcJF4GiIuyPEi4CRUXYHy2F74s4yXEIfRjfUgkJCQkJCQlnFa2O0h/GlzGyIrxpJrweX8bIivCmmfB6fBkjK8Kb5jcKJ8cmOSYv0ZeavBzynNCEhISEhIT/sXCZY5n1jZsJCQkJCQkJ3/B/wFGg6Ho0lZ0nJCQkJCQknAmj9JOpSYzRFyckJCQkJCR8p7BLP7kefVbCF2OHq9HCqAhfNEeBCAl3RfiiOQr0jwgntby6PHpYSJgfWwbqjhISRseWgbqjhITRsWWg7mgp7OoQOsoxaS4TbucJL1+65jLhdp7w8qVrLhNu5wkvX7rmMuF2nvDypWvu6hdwJWEkXvHKCAAAAABJRU5ErkJggg==',
     
@@ -179,9 +201,9 @@ export default {
 
   created () {
     this.emloyeeId = CookieGet("POS_USERID")
-    if (this.billsList.length <= 0) {
+    // if (this.billsList.length <= 0) {
       this.setBills()
-    }
+    // }
     if (this.category.length <= 0) {
       this.setFoodCategory()
     }
@@ -229,11 +251,25 @@ export default {
       this.isShowPayCode = true
     },
 
+    openOtherPay (item) {
+      this.closeOtherPayId = item.id
+      this.isShowOtherPay = true
+    },
+
+    closePayBills () {
+      this.userCloseBills(this.closeOtherPayId).then(() => {
+        this.isShowOtherPay = false
+        this.userIdx = ""
+        this.setBills()
+      })
+    },
+
     ...mapActions("bills", [
       "setBills",
       "setFoodCategory",
       "removeBillsGoods",
-      "addBillsGoods"
+      "addBillsGoods",
+      "userCloseBills"
     ])
   },
 };
